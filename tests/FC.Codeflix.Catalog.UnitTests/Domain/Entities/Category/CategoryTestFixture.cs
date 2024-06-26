3b56b6f82f0225141295e63e3c6003d885eb32af
RafaelLimaTk <rafamano123.rl@gmail.com>
2024-06-25 21:58:18 -0300

    Adiciona fixture de teste para Category

---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entities/Category/CategoryTestFixture.cs
@@ -0,0 +1,15 @@
+﻿namespace FC.Codeflix.Catalog.UnitTests.Domain.Entities.Category;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entities;
+public class CategoryTestFixture
+{
+    public DomainEntity.Category GetValidCategory()
+    => new(
+        GetValidCategoryName(),
+        GetValidCategoryDescription()
+    );
+}
+
+[CollectionDefinition(nameof(CategoryTestFixture))]
+public class CategoryTestFixtureCollection
+    : ICollectionFixture<CategoryTestFixture>
+{ }
