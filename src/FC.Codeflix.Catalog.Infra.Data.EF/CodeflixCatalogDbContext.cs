e2b15c0d9ebf8064b4bd8919e60be42b996342df
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-05 07:59:37 -0300

    Correção do tipo de projeto da aplicação, a adição dos projetos de testes de integração e infraestrutura de dados, além da configuração do DbContext com o Entity Framework Core.

---
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/CodeflixCatalogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FC.Codeflix.Catalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.Codeflix.Catalog.Infra.Data.EF;
+public class CodeflixCatalogDbContext : DbContext
+{
+    public CodeflixCatalogDbContext(DbContextOptions<CodeflixCatalogDbContext> options)
+        : base(options)
+    { }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CodeflixCatalogDbContext).Assembly);
+    }
+}
---
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -0,0 +1,61 @@
+﻿using FC.Codeflix.Catalog.Application.Exceptions;
+using FC.Codeflix.Catalog.Domain.Entities;
+using FC.Codeflix.Catalog.Domain.Interfaces;
+using FC.Codeflix.Catalog.Domain.SeedWorks.SearchableRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+public class CategoryRepository
+    : ICategoryRepository
+{
+    private readonly CodeflixCatalogDbContext _context;
+    private DbSet<Category> _categories
+        => _context.Set<Category>();
+
+    public CategoryRepository(CodeflixCatalogDbContext context)
+    => _context = context;
+
+    public async Task Insert(
+    Category aggregate,
+    CancellationToken cancellationToken
+    )
+    => await _categories.AddAsync(aggregate, cancellationToken);
+
+    public async Task<Category> Get(Guid id, CancellationToken cancellationToken)
+    {
+        var category = await _categories.AsNoTracking().FirstOrDefaultAsync(
+            x => x.Id == id,
+            cancellationToken
+        );
+        NotFoundException.ThrowIfNull(category, $"Category '{id}' not found.");
+        return category!;
+    }
+
+    public Task Update(Category aggregate, CancellationToken _)
+    => Task.FromResult(_categories.Update(aggregate));
+
+    public Task Delete(Category aggregate, CancellationToken _)
+        => Task.FromResult(_categories.Remove(aggregate));
+
+    public Task<SearchResponse<Category>> Search(SearchRequest input, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    //public async Task<SearchResponse<Category>> Search(
+    //SearchRequest input,
+    //CancellationToken cancellationToken)
+    //{
+    //    var toSkip = (input.Page - 1) * input.PerPage;
+    //    var query = _categories.AsNoTracking();
+    //    query = AddOrderToQuery(query, input.OrderBy, input.Order);
+    //    if (!String.IsNullOrWhiteSpace(input.Search))
+    //        query = query.Where(x => x.Name.Contains(input.Search));
+    //    var total = await query.CountAsync();
+    //    var items = await query
+    //        .Skip(toSkip)
+    //        .Take(input.PerPage)
+    //        .ToListAsync();
+    //    return new(input.Page, input.PerPage, total, items);
+    //}
+}
