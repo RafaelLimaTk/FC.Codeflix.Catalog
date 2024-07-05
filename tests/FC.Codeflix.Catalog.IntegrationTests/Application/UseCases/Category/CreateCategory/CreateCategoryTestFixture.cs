9bc090af95cbcb7cfee1702eb47efa2f3504e026
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-05 17:27:30 -0300

    Adiciona testes e utilitários para categorias

---
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,57 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+
+namespace FC.Codeflix.Catalog.IntegrationTests.Application.UseCases.Category.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryTestFixture))]
+public class CreateCategoryTestFixtureCollection
+    : ICollectionFixture<CreateCategoryTestFixture>
+{ }
+public class CreateCategoryTestFixture
+        : CategoryUseCasesBaseFixture
+{
+    public CreateCategoryRequest GetInput()
+    {
+        var category = GetExampleCategory();
+        return new CreateCategoryRequest(
+            category.Name,
+            category.Description,
+            category.IsActive
+        );
+    }
+
+    public CreateCategoryRequest GetInvalidInputTooLongName()
+    {
+        var invalidInputTooLongName = GetInput();
+        var tooLongNameForCategory = Faker.Commerce.ProductName();
+        while (tooLongNameForCategory.Length <= 255)
+            tooLongNameForCategory = $"{tooLongNameForCategory} {Faker.Commerce.ProductName()}";
+        invalidInputTooLongName.Name = tooLongNameForCategory;
+        return invalidInputTooLongName;
+    }
+
+    public CreateCategoryRequest GetInvalidInputCategoryNull()
+    {
+        var invalidInputDescriptionNull = GetInput();
+        invalidInputDescriptionNull.Description = null!;
+        return invalidInputDescriptionNull;
+    }
+
+    public CreateCategoryRequest GetInvalidInputTooLongDescription()
+    {
+        var invalidInputTooLongDescription = GetInput();
+        var tooLongDescriptionForCategory = Faker.Commerce.ProductDescription();
+        while (tooLongDescriptionForCategory.Length <= 10_000)
+            tooLongDescriptionForCategory = $"{tooLongDescriptionForCategory} {Faker.Commerce.ProductDescription()}";
+        invalidInputTooLongDescription.Description = tooLongDescriptionForCategory;
+        return invalidInputTooLongDescription;
+    }
+
+    public CreateCategoryRequest GetInvalidInputShortName()
+    {
+        var invalidInputShortName = GetInput();
+        invalidInputShortName.Name =
+            invalidInputShortName.Name.Substring(0, 2);
+        return invalidInputShortName;
+    }
+}
