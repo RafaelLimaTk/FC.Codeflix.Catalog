ec398c82a08539c8b36baf0930b1d61c501986a6
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-05 16:38:52 -0300

    Atualizações e testes no CategoryRepository

--- src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
+++ src/FC.Codeflix.Catalog.Infra.Data.EF/Repositories/CategoryRepository.cs
@@ -37,25 +37,42 @@ public class CategoryRepository
     public Task Delete(Category aggregate, CancellationToken _)
         => Task.FromResult(_categories.Remove(aggregate));
 
-    public Task<SearchResponse<Category>> Search(SearchRequest input, CancellationToken cancellationToken)
+    public async Task<SearchResponse<Category>> Search(
+    SearchRequest input,
+    CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var toSkip = (input.Page - 1) * input.PerPage;
+        var query = _categories.AsNoTracking();
+        query = AddOrderToQuery(query, input.OrderBy, input.Order);
+        if (!String.IsNullOrWhiteSpace(input.Search))
+            query = query.Where(x => x.Name.Contains(input.Search));
+        var total = await query.CountAsync();
+        var items = await query
+            .Skip(toSkip)
+            .Take(input.PerPage)
+            .ToListAsync();
+        return new(input.Page, input.PerPage, total, items);
     }
 
-    //public async Task<SearchResponse<Category>> Search(
-    //SearchRequest input,
-    //CancellationToken cancellationToken)
-    //{
-    //    var toSkip = (input.Page - 1) * input.PerPage;
-    //    var query = _categories.AsNoTracking();
-    //    query = AddOrderToQuery(query, input.OrderBy, input.Order);
-    //    if (!String.IsNullOrWhiteSpace(input.Search))
-    //        query = query.Where(x => x.Name.Contains(input.Search));
-    //    var total = await query.CountAsync();
-    //    var items = await query
-    //        .Skip(toSkip)
-    //        .Take(input.PerPage)
-    //        .ToListAsync();
-    //    return new(input.Page, input.PerPage, total, items);
-    //}
+    private IQueryable<Category> AddOrderToQuery(
+        IQueryable<Category> query,
+        string orderProperty,
+        SearchOrder order
+    )
+    {
+        var orderedQuery = (orderProperty.ToLower(), order) switch
+        {
+            ("name", SearchOrder.Asc) => query.OrderBy(x => x.Name)
+                .ThenBy(x => x.Id),
+            ("name", SearchOrder.Desc) => query.OrderByDescending(x => x.Name)
+                .ThenByDescending(x => x.Id),
+            ("id", SearchOrder.Asc) => query.OrderBy(x => x.Id),
+            ("id", SearchOrder.Desc) => query.OrderByDescending(x => x.Id),
+            ("createdat", SearchOrder.Asc) => query.OrderBy(x => x.CreatedAt),
+            ("createdat", SearchOrder.Desc) => query.OrderByDescending(x => x.CreatedAt),
+            _ => query.OrderBy(x => x.Name)
+                .ThenBy(x => x.Id)
+        };
+        return orderedQuery;
+    }
 }
