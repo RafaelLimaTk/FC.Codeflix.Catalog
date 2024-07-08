f0e7c51a9ac6a7f4e953f4da67b0c77954802498
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-08 09:22:34 -0300

    Refatoração da configuração da API

---
+++ src/FC.Codeflix.Catalog.Api/Configurations/ApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using FC.Codeflix.Catalog.Application.Utils;
+
+namespace FC.Codeflix.Catalog.Api.Configurations;
+
+public static class ApiConfig
+{
+    public static void AddConfigureApi(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddHttpContextAccessor();
+        services.AddSwaggerConfiguration();
+        services.AddRegister();
+        services.AddUseCases();
+        services.AddEndpointsApiExplorer();
+    }
+
+    public static void UseConfigureApi(this WebApplication app, IConfiguration configuration)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwaggerConfiguration();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+    }
+}
---
+++ src/FC.Codeflix.Catalog.Api/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,20 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Domain.Interfaces;
+using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
+
+namespace FC.Codeflix.Catalog.Api.Configurations;
+
+public static class DependencyInjectionConfig
+{
+    public static void AddRegister(this IServiceCollection services)
+    {
+        #region Repositories
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        #endregion
+
+        #region UnitOfWork
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        #endregion
+    }
+}
