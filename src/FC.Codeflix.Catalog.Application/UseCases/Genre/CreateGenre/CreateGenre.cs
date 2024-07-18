2e88a933c5adf732ceb665288301a7df0f40507e
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-18 07:18:44 -0300

    Criando o use case de genre para criar um novo genre

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Genre/CreateGenre/CreateGenre.cs
@@ -0,0 +1,10 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Genre.CreateGenre;
+public class CreateGenre : ICreateGenre
+{
+    public Task<GenreModelResponse> Handle(CreateGenreRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Genre/CreateGenre/CreateGenreRequest.cs
@@ -0,0 +1,22 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
+using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Genre.CreateGenre;
+
+public class CreateGenreRequest : IRequest<GenreModelResponse>
+{
+    public string Name { get; set; }
+    public bool IsActive { get; set; }
+    public List<Guid>? CategoriesId { get; set; }
+
+    public CreateGenreRequest(
+        string name,
+        bool isActive,
+        List<Guid>? categoriesId = null
+    )
+    {
+        Name = name;
+        IsActive = isActive;
+        CategoriesId = categoriesId;
+    }
+}
