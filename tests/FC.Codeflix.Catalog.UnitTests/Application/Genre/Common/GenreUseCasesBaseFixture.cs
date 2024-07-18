f97d299ce6e74efad04fa11e40b37f14f651801b
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-18 07:06:56 -0300

    Melhorando o base fixture de genre

--- tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/Common/GenreUseCasesBaseFixture.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Genre/Common/GenreUseCasesBaseFixture.cs
@@ -41,4 +41,35 @@ public class GenreUseCasesBaseFixture
             .Range(1, count ?? (new Random()).Next(1, 10))
             .Select(_ => Guid.NewGuid())
             .ToList();
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription =
+            Faker.Commerce.ProductDescription();
+        if (categoryDescription.Length > 10_000)
+            categoryDescription =
+                categoryDescription[..10_000];
+        return categoryDescription;
+    }
+
+    public DomainEntity.Category GetExampleCategory()
+        => new(
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+
+    public List<DomainEntity.Category> GetExampleCategoriesList(int count = 5)
+        => Enumerable.Range(0, count).Select(_ => GetExampleCategory())
+            .ToList();
 }
