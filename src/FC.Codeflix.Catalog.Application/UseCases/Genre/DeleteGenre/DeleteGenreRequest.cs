3c20f62303c1e2caa2dfdea568ccf9e1dadcdc8c
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-20 17:35:22 -0300

    Adiciona funcionalidade de deletar gênero

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Genre/DeleteGenre/DeleteGenreRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.Genre.DeleteGenre;
+public class DeleteGenreRequest
+    : IRequest
+{
+    public DeleteGenreRequest(Guid id) => Id = id;
+
+    public Guid Id { get; set; }
+}
