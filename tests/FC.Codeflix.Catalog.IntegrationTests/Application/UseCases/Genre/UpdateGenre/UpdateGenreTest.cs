a1b6095d4975265889b79fd94acc349e62140ea5
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-21 16:36:33 -0300

    Adiciona teste para validar a atualização de gêneros sem criar novas relações com categorias.

--- tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/UpdateGenre/UpdateGenreTest.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/UpdateGenre/UpdateGenreTest.cs
@@ -126,4 +126,68 @@ public class UpdateGenreTest
             .ToListAsync();
         relatedcategoryIdsFromDb.Should().BeEquivalentTo(input.CategoriesIds);
     }
+
+    [Fact(DisplayName = nameof(UpdateGenreWithoutNewCategoriesRelations))]
+    [Trait("Integration/Application", "UpdateGenre - Use Cases")]
+    public async Task UpdateGenreWithoutNewCategoriesRelations()
+    {
+        List<DomainEntity.Category> exampleCategories = _fixture.GetExampleCategoriesList(10);
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        CodeflixCatalogDbContext arrangeDbContext = _fixture.CreateDbContext();
+        DomainEntity.Genre targetGenre = exampleGenres[5];
+        List<DomainEntity.Category> relatedCategories = exampleCategories.GetRange(0, 5);
+        relatedCategories.ForEach(category => targetGenre.AddCategory(category.Id));
+        List<GenresCategories> relations = targetGenre.Categories
+            .Select(categoryId => new GenresCategories(categoryId, targetGenre.Id))
+            .ToList();
+        await arrangeDbContext.AddRangeAsync(exampleGenres);
+        await arrangeDbContext.AddRangeAsync(exampleCategories);
+        await arrangeDbContext.AddRangeAsync(relations);
+        await arrangeDbContext.SaveChangesAsync();
+        CodeflixCatalogDbContext actDbContext = _fixture.CreateDbContext(true);
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging();
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        //var eventPublisher = new DomainEventPublisher(serviceProvider);
+        var unitOfWork = new UnitOfWork(actDbContext);
+        UseCase.UpdateGenre updateGenre = new UseCase.UpdateGenre(
+            new GenreRepository(actDbContext),
+            unitOfWork,
+            new CategoryRepository(actDbContext)
+        );
+        UpdateGenreRequest input = new UpdateGenreRequest(
+            targetGenre.Id,
+            _fixture.GetValidGenreName(),
+            !targetGenre.IsActive
+        );
+
+        GenreModelResponse output = await updateGenre.Handle(
+            input,
+            CancellationToken.None
+        );
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(targetGenre.Id);
+        output.Name.Should().Be(input.Name);
+        output.IsActive.Should().Be((bool)input.IsActive!);
+        output.Categories.Should().HaveCount(relatedCategories.Count);
+        List<Guid> expectedRelatedCategoryIds = relatedCategories
+            .Select(category => category.Id).ToList();
+        List<Guid> relatedCategoryIdsFromOutput =
+            output.Categories.Select(relatedCategory => relatedCategory.Id).ToList();
+        relatedCategoryIdsFromOutput.Should().BeEquivalentTo(expectedRelatedCategoryIds);
+        CodeflixCatalogDbContext assertDbContext = _fixture.CreateDbContext(true);
+        DomainEntity.Genre? genreFromDb =
+            await assertDbContext.Genres.FindAsync(targetGenre.Id);
+        genreFromDb.Should().NotBeNull();
+        genreFromDb!.Id.Should().Be(targetGenre.Id);
+        genreFromDb.Name.Should().Be(input.Name);
+        genreFromDb.IsActive.Should().Be((bool)input.IsActive!);
+        List<Guid> relatedcategoryIdsFromDb = await assertDbContext
+            .GenresCategories.AsNoTracking()
+            .Where(relation => relation.GenreId == input.Id)
+            .Select(relation => relation.CategoryId)
+            .ToListAsync();
+        relatedcategoryIdsFromDb.Should().BeEquivalentTo(expectedRelatedCategoryIds);
+    }
 }
