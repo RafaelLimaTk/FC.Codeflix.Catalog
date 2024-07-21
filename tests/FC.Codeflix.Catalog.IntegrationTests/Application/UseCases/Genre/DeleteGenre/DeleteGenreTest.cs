2f23606580a92350252a2b14e045ceeb335f4d04
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-21 16:45:35 -0300

    Adiciona teste e dependências para DeleteGenreWithRelations

--- tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/DeleteGenre/DeleteGenreTest.cs
+++ tests/FC.Codeflix.Catalog.IntegrationTests/Application/UseCases/Genre/DeleteGenre/DeleteGenreTest.cs
@@ -1,6 +1,8 @@
 ﻿using FC.Codeflix.Catalog.Infra.Data.EF;
+using FC.Codeflix.Catalog.Infra.Data.EF.Models;
 using FC.Codeflix.Catalog.Infra.Data.EF.Repositories;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using UseCase = FC.Codeflix.Catalog.Application.UseCases.Genre.DeleteGenre;
 
@@ -40,5 +42,47 @@ public class DeleteGenreTest
         var assertDbContext = _fixture.CreateDbContext(true);
         var genreFromDb = await assertDbContext.Genres.FindAsync(targetGenre.Id);
         genreFromDb.Should().BeNull();
+    }
+
+    [Fact(DisplayName = nameof(DeleteGenreWithRelations))]
+    [Trait("Integration/Application", "DeleteGenre - Use Cases")]
+    public async Task DeleteGenreWithRelations()
+    {
+        var genresExampleList = _fixture.GetExampleListGenres(10);
+        var targetGenre = genresExampleList[5];
+        var exampleCategories = _fixture.GetExampleCategoriesList(5);
+        var dbArrangeContext = _fixture.CreateDbContext();
+        await dbArrangeContext.Genres.AddRangeAsync(genresExampleList);
+        await dbArrangeContext.Categories.AddRangeAsync(exampleCategories);
+        await dbArrangeContext.GenresCategories.AddRangeAsync(
+            exampleCategories.Select(category =>
+                new GenresCategories(
+                    category.Id,
+                    targetGenre.Id
+                )
+            )
+        );
+        await dbArrangeContext.SaveChangesAsync();
+        var actDbContext = _fixture.CreateDbContext(true);
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging();
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        //var eventPublisher = new DomainEventPublisher(serviceProvider);
+        var unitOfWork = new UnitOfWork(actDbContext);
+        var useCase = new UseCase.DeleteGenre(
+            new GenreRepository(actDbContext),
+            unitOfWork
+        );
+        var input = new UseCase.DeleteGenreRequest(targetGenre.Id);
+
+        await useCase.Handle(input, CancellationToken.None);
+
+        var assertDbContext = _fixture.CreateDbContext(true);
+        var genreFromDb = await assertDbContext.Genres.FindAsync(targetGenre.Id);
+        genreFromDb.Should().BeNull();
+        var relations = await assertDbContext.GenresCategories.AsNoTracking()
+            .Where(relation => relation.GenreId == targetGenre.Id)
+            .ToListAsync();
+        relations.Should().HaveCount(0);
     }
 }
