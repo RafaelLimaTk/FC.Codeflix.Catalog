12d5e3644311897d314b3c64eff20765b5241803
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-22 13:51:51 -0300

    Implementa deleção de gênero por ID

---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/DeleteGenre/DeleteGenreTestApi.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entities;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.DeleteGenre;
+
+[Collection(nameof(DeleteGenreTestApiFixture))]
+public class DeleteGenreTestApi : IDisposable
+{
+    private readonly DeleteGenreTestApiFixture _fixture;
+
+    public DeleteGenreTestApi(DeleteGenreTestApiFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(DeleteGenre))]
+    [Trait("EndToEnd/Api", "Genre/DeleteGenre - Endpoints")]
+    public async Task DeleteGenre()
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        var targetGenre = exampleGenres[5];
+        await _fixture.GenrePersistence.InsertList(exampleGenres);
+
+        var (response, output) = await _fixture.ApiClient
+            .Delete<object>($"/genres/{targetGenre.Id}");
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status204NoContent);
+        output.Should().BeNull();
+        DomainEntity.Genre? genreDb = await _fixture.GenrePersistence.GetById(targetGenre.Id);
+        genreDb.Should().BeNull();
+    }
+
+    public void Dispose() => _fixture.CleanPersistence();
+}
