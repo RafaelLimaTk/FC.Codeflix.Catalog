0e42f628584a4d726d0dfe242418f99ca72378b1
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-22 13:45:02 -0300

    Configuração da injeção de dependência para IGenreRepository, criação do GenresController e implementação de testes de ponta a ponta

---
+++ src/FC.Codeflix.Catalog.Api/Controllers/GenresController.cs
@@ -0,0 +1,29 @@
+﻿using FC.Codeflix.Catalog.Api.ApiModels.Response;
+using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Genre.GetGenre;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FC.Codeflix.Catalog.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GenresController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public GenresController(IMediator mediator)
+        => _mediator = mediator;
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(ApiResponse<GenreModelResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById(
+        [FromRoute] Guid id,
+        CancellationToken cancellationToken
+    )
+    {
+        var output = await _mediator.Send(new GetGenreRequest(id), cancellationToken);
+        return Ok(new ApiResponse<GenreModelResponse>(output));
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/GetGenre/GetGenreApiTest.cs
@@ -0,0 +1,39 @@
+﻿using FC.Codeflix.Catalog.Api.ApiModels.Response;
+using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entities;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.GetGenre;
+
+[Collection(nameof(GetGenreApiTestFixture))]
+public class GetGenreApiTest : IDisposable
+{
+    private GetGenreApiTestFixture _fixture;
+
+    public GetGenreApiTest(GetGenreApiTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(GetGenre))]
+    [Trait("EndToEnd/API", "Genre/GetGenre - Endpoints")]
+    public async Task GetGenre()
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        var targetGenre = exampleGenres[5];
+        await _fixture.GenrePersistence.InsertList(exampleGenres);
+
+        var (response, output) = await _fixture.ApiClient
+            .Get<ApiResponse<GenreModelResponse>>($"/genres/{targetGenre.Id}");
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Data.Id.Should().Be(targetGenre.Id);
+        output.Data.Name.Should().Be(targetGenre.Name);
+        output.Data.IsActive.Should().Be(targetGenre.IsActive);
+    }
+
+
+    public void Dispose() => _fixture.CleanPersistence();
+}
