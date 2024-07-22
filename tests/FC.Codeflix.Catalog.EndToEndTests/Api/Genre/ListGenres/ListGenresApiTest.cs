5584819adf6057f016fa9ad7b743c22381d14ef5
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-22 14:28:31 -0300

    Adiciona testes E2E para listagem de gêneros

---
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/ListGenres/ListGenresApiTest.cs
@@ -0,0 +1,52 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
+using FC.Codeflix.Catalog.Application.UseCases.Genre.ListGenres;
+using FC.Codeflix.Catalog.EndToEndTests.Extensions.DateTime;
+using FC.Codeflix.Catalog.EndToEndTests.Models;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using DomainEntity = FC.Codeflix.Catalog.Domain.Entities;
+
+namespace FC.Codeflix.Catalog.EndToEndTests.Api.Genre.ListGenres;
+
+[Collection(nameof(ListGenresApiTestFixture))]
+public class ListGenresApiTest
+{
+    private readonly ListGenresApiTestFixture _fixture;
+
+    public ListGenresApiTest(ListGenresApiTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(List))]
+    [Trait("EndToEnd/Api", "Genre/ListGenres - Endpoints")]
+    public async Task List()
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        await _fixture.GenrePersistence.InsertList(exampleGenres);
+        var input = new ListGenresRequest();
+        input.Page = 1;
+        input.PerPage = exampleGenres.Count;
+
+        var (response, output) = await _fixture.ApiClient
+            .Get<TestApiResponseList<GenreModelResponse>>("/genres", input);
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Meta.Should().NotBeNull();
+        output.Data.Should().NotBeNull();
+        output.Meta!.Total.Should().Be(exampleGenres.Count);
+        output.Meta.CurrentPage.Should().Be(input.Page);
+        output.Meta.PerPage.Should().Be(input.PerPage);
+        output.Data!.Count.Should().Be(exampleGenres.Count);
+        output.Data.ToList().ForEach(outputItem =>
+        {
+            var exampleItem = exampleGenres.Find(x => x.Id == outputItem.Id);
+            exampleItem.Should().NotBeNull();
+            outputItem.Name.Should().Be(exampleItem!.Name);
+            outputItem.IsActive.Should().Be(exampleItem.IsActive);
+            outputItem.CreatedAt.TrimMillisseconds()
+                .Should().Be(exampleItem.CreatedAt.TrimMillisseconds());
+        });
+    }
+}
