922800b7bff669086e6189f3a4c83b3e235408ec
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-22 13:46:08 -0300

    Adiciona teste de NotFound em GetGenreApiTest

--- tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/GetGenre/GetGenreApiTest.cs
+++ tests/FC.Codeflix.Catalog.EndToEndTests/Api/Genre/GetGenre/GetGenreApiTest.cs
@@ -2,6 +2,7 @@
 using FC.Codeflix.Catalog.Application.UseCases.Genre.Common;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using DomainEntity = FC.Codeflix.Catalog.Domain.Entities;
 
@@ -34,6 +35,23 @@ public class GetGenreApiTest : IDisposable
         output.Data.IsActive.Should().Be(targetGenre.IsActive);
     }
 
+    [Fact(DisplayName = nameof(NotFound))]
+    [Trait("EndToEnd/API", "Genre/GetGenre - Endpoints")]
+    public async Task NotFound()
+    {
+        List<DomainEntity.Genre> exampleGenres = _fixture.GetExampleListGenres(10);
+        var randomGuid = Guid.NewGuid();
+        await _fixture.GenrePersistence.InsertList(exampleGenres);
+
+        var (response, output) = await _fixture.ApiClient
+            .Get<ProblemDetails>($"/genres/{randomGuid}");
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status404NotFound);
+        output.Should().NotBeNull();
+        output!.Type.Should().Be("NotFound");
+        output.Detail.Should().Be($"Genre '{randomGuid}' not found.");
+    }
 
     public void Dispose() => _fixture.CleanPersistence();
 }
