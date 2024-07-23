960c4b21a40bc2b826f5b4e92b432bd5879e856f
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-07-23 07:09:16 -0300

    Adiciona caso de uso GetCastMember

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/CastMember/GetCastMember/GetCastMemberRequest.cs
@@ -0,0 +1,10 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.CastMember.Common;
+using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.CastMember.GetCastMember;
+public class GetCastMemberRequest
+    : IRequest<CastMemberModelResponse>
+{
+    public Guid Id { get; private set; }
+    public GetCastMemberRequest(Guid id) => Id = id;
+}
