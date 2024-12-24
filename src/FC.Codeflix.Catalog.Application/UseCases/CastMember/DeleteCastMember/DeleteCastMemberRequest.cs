f4ba3b688bf528ed63514e7cb8091dd8ae7966fc
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-12-24 10:39:24 -0300

    Implementação da exclusão do CastMember

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/CastMember/DeleteCastMember/DeleteCastMemberRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.CastMember.DeleteCastMember;
+public class DeleteCastMemberRequest
+    : IRequest
+{
+    public Guid Id { get; private set; }
+    public DeleteCastMemberRequest(Guid id) => Id = id;
+}
