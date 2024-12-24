bda65f65c0d1719af83fbae23b24f2283ecc7f8d
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2024-12-24 10:50:27 -0300

    Adiciona caso de uso e testes para atualização do CastMember

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/CastMember/UpdateCastMember/UpdateCastMemberRequest.cs
@@ -0,0 +1,18 @@
+﻿using FC.Codeflix.Catalog.Application.UseCases.CastMember.Common;
+using FC.Codeflix.Catalog.Domain.Enums;
+using MediatR;
+
+namespace FC.Codeflix.Catalog.Application.UseCases.CastMember.UpdateCastMember;
+public class UpdateCastMemberRequest : IRequest<CastMemberModelResponse>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public CastMemberType Type { get; set; }
+
+    public UpdateCastMemberRequest(Guid id, string name, CastMemberType type)
+    {
+        this.Id = id;
+        Name = name;
+        Type = type;
+    }
+}
