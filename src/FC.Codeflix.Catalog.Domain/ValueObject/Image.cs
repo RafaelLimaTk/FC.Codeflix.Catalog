7d5b55118f04fd64e46521db2839b6d87072df91
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2025-03-11 09:00:44 -0300

    Implementa Value Object e testes para a classe Image

---
+++ src/FC.Codeflix.Catalog.Domain/ValueObject/Image.cs
@@ -0,0 +1,16 @@
+﻿
+namespace FC.Codeflix.Catalog.Domain.ValueObject;
+
+public class Image : SeedWorks.ValueObject
+{
+    public string Path { get; }
+
+    public Image(string path) => Path = path;
+
+    public override bool Equals(SeedWorks.ValueObject? other) =>
+        other is Image image &&
+        Path == image.Path;
+
+    protected override int GetCustomHashCode()
+        => HashCode.Combine(Path);
+}
