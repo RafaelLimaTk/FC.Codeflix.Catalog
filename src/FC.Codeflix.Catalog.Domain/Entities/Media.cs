74b8373a0f64b75d1ae07a1384e2ff2396a15ed4
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2025-03-11 18:26:59 -0300

    Adicionar classes Media e MediaStatus com testes

---
+++ src/FC.Codeflix.Catalog.Domain/Entities/Media.cs
@@ -0,0 +1,16 @@
+﻿using FC.Codeflix.Catalog.Domain.Enums;
+
+namespace FC.Codeflix.Catalog.Domain.Entities;
+
+public class Media : SeedWorks.Entity
+{
+    public string FilePath { get; private set; }
+    public string? EncodedPath { get; private set; }
+    public MediaStatus Status { get; private set; }
+
+    public Media(string filePath) : base()
+    {
+        FilePath = filePath;
+        Status = MediaStatus.Pending;
+    }
+}
---
+++ tests/FC.Codeflix.Catalog.UnitTests/Domain/Entities/Video/MediaTest.cs
@@ -0,0 +1,27 @@
+﻿using FC.Codeflix.Catalog.Domain.Entities;
+using FC.Codeflix.Catalog.Domain.Enums;
+using FluentAssertions;
+
+namespace FC.Codeflix.Catalog.UnitTests.Domain.Entities.Video;
+
+[Collection(nameof(VideoTestFixture))]
+public class MediaTest
+{
+    private readonly VideoTestFixture _fixture;
+
+    public MediaTest(VideoTestFixture fixture)
+        => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(Instantiate))]
+    [Trait("Domain", "Media - Entities")]
+    public void Instantiate()
+    {
+        var expectedFilePath = _fixture.GetValidMediaPath();
+
+        var media = new Media(expectedFilePath);
+
+        media.Id.Should().NotBe(default(Guid));
+        media.FilePath.Should().Be(expectedFilePath);
+        media.Status.Should().Be(MediaStatus.Pending);
+    }
+}
