a3ee68bb8f282fdc5db3be25e1106d9b4730879d
IATec - Rafael Lima Pinheiro <rafael.lima@iatec.com>
2025-07-10 18:18:25 -0300

    Adicionar teste GetWithAllProperties em GetVideoTest

--- tests/FC.Codeflix.Catalog.UnitTests/Application/Video/GetVideo/GetVideoTest.cs
+++ tests/FC.Codeflix.Catalog.UnitTests/Application/Video/GetVideo/GetVideoTest.cs
@@ -43,4 +43,47 @@ public class GetVideoTest
         output.Opened.Should().Be(exampleVideo.Opened);
         repositoryMock.VerifyAll();
     }
+
+    [Fact(DisplayName = nameof(GetWithAllProperties))]
+    [Trait("Application", "GetVideo - Use Cases")]
+    public async Task GetWithAllProperties()
+    {
+        var exampleVideo = _fixture.GetValidVideoWithAllProperties();
+        var repositoryMock = new Mock<IVideoRepository>();
+        repositoryMock.Setup(x => x.Get(
+            It.Is<Guid>(id => id == exampleVideo.Id),
+            It.IsAny<CancellationToken>())
+        ).ReturnsAsync(exampleVideo);
+        var useCase = new UseCase.GetVideo(repositoryMock.Object,
+            Mock.Of<ICategoryRepository>(),
+            Mock.Of<IGenreRepository>());
+        var input = new UseCase.GetVideoRequest(exampleVideo.Id);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        output.Should().NotBeNull();
+        output.Id.Should().Be(exampleVideo.Id);
+        output.CreatedAt.Should().Be(exampleVideo.CreatedAt);
+        output.Title.Should().Be(exampleVideo.Title);
+        output.Published.Should().Be(exampleVideo.Published);
+        output.Description.Should().Be(exampleVideo.Description);
+        output.Duration.Should().Be(exampleVideo.Duration);
+        output.Rating.Should().Be(exampleVideo.Rating.ToStringSignal());
+        output.YearLaunched.Should().Be(exampleVideo.YearLaunched);
+        output.Opened.Should().Be(exampleVideo.Opened);
+        output.ThumbFileUrl.Should().Be(exampleVideo.Thumb!.Path);
+        output.ThumbHalfFileUrl.Should().Be(exampleVideo.ThumbHalf!.Path);
+        output.BannerFileUrl.Should().Be(exampleVideo.Banner!.Path);
+        output.VideoFileUrl.Should().Be(exampleVideo.Media!.FilePath);
+        output.TrailerFileUrl.Should().Be(exampleVideo.Trailer!.FilePath);
+        var outputItemCategoryIds = output.Categories
+            .Select(categoryDto => categoryDto.Id).ToList();
+        outputItemCategoryIds.Should().BeEquivalentTo(exampleVideo.Categories);
+        var outputItemGenresIds = output.Genres
+            .Select(dto => dto.Id).ToList();
+        outputItemGenresIds.Should().BeEquivalentTo(exampleVideo.Genres);
+        var outputItemCastMembersIds = output.CastMembers
+            .Select(dto => dto.Id).ToList();
+        repositoryMock.VerifyAll();
+    }
 }
